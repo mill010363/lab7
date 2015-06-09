d9a8bd692413214bd4e78ab2fda8cb74b8340b20
mill010363 <jack98526@gmail.com>
2015-06-09 11:58:51 +0800

    lab7
    lab7

---
+++ genmino.h
@@ -0,0 +1,13 @@
+#ifndef GENMINO_H
+#define GENMINO_H
+
+#include "MinoI.h"
+#include "MinoS.h"
+#include "MinoZ.h"
+#include "MinoL.h"
+#include "MinoJ.h"
+#include "MinoO.h"
+
+Mino* genMino();
+
+#endif
